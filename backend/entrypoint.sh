39c89ba465d10ad796c57fcde5bd31a8aee1f574
Misha <misha012004@gmail.com>
2024-11-25 17:18:08 +0300

    script 3.2

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -9,4 +9,7 @@ python manage.py collectstatic --noinput
 echo "Runserver"
 gunicorn --bind 0.0.0.0:9000 kittygram_backend.wsgi
 
+echo "Copy"
+cp -r /app/collected_static/. /backend_static/static/
+
 exec "$@"
