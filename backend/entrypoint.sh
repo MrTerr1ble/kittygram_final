76c2919dc61585864fc00a125b994cd3c803aec5
Misha <misha012004@gmail.com>
2024-11-25 15:52:51 +0300

    script 2.6

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+
 echo "Apply database migrations"
 python manage.py migrate
 
 echo "Collect static files"
 python manage.py collectstatic --noinput
 
-python gunicorn --bind 0.0.0.0:9000 kittygram_backend.wsgi
+echo "Starting Gunicorn server"
+exec gunicorn --bind 0.0.0.0:9000 kittygram_backend.wsgi:application
