b985aefdaaf5e40bc01a375cd80df766de11fb05
Misha <misha012004@gmail.com>
2024-11-27 12:17:47 +0300

    script 3.3

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -6,10 +6,4 @@ python manage.py migrate
 echo "Collect static files"
 python manage.py collectstatic --noinput
 
-echo "Runserver"
-gunicorn --bind 0.0.0.0:9000 kittygram_backend.wsgi
-
-echo "Copy"
-cp -r /app/collected_static/. /backend_static/static/
-
 exec "$@"
