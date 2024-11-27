194d741c4f1553b2b182f3ec1fad123c9bb6abcf
Misha <misha012004@gmail.com>
2024-11-27 12:34:18 +0300

    script 3.4(add dir in container)

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -3,6 +3,9 @@
 echo "Apply database migrations"
 python manage.py migrate
 
+echo "Ensure collected_static directory exists"
+mkdir -p /app/collected_static
+
 echo "Collect static files"
 python manage.py collectstatic --noinput
 
